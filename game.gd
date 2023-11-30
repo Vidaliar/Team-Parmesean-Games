a89a3fc3da11a4d8e24109990d59941ea1adbc41
horvj <horvathjack1@gmail.com>
2023-11-29 19:37:57 -0500

    update

--- game.gd
+++ game.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	load("res://Cube/cube.tscn")
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
